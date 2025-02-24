fdfe8e23770213a3c84201c7f43b2a4e59cb8aa3
Juan Orellana <juan.orellana2@catolica.edu.sv>
2025-02-24 17:38:02 -0600

    Agregar archivos de proyecto.

---
+++ Models/parqueoContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace P01_2022_SS_650_2021_OF_601.Models
+{
+    public class parqueoContext : DbContext
+    {
+        public parqueoContext(DbContextOptions<parqueoContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using P01_2022_SS_650_2021_OF_601.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+builder.Services.AddDbContext<parqueoContext>(options =>
+    options.UseSqlServer(
+            builder.Configuration.GetConnectionString("parqueoConnection")
+        )
+);
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
