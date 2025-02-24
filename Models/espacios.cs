749314c5c58f322bb36c77ebd55eb55141ae1991
Rafael Sandoval <rafael.sandoval@catolica.edu.sv>
2025-02-24 17:49:13 -0600

    creacion de las clases espacios y reservas

---
+++ Models/espacios.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P01_2022_SS_650_2021_OF_601.Models
+{
+    public class espacios
+    {
+        [Key]
+        public int id_espacios { get; set; }
+        public string numero { get; set; }
+        public string ubicacion { get; set; }
+        public int costo_hora { get; set; }
+        public bool estado { get; set; }
+        public int id_sucursal { get; set; }
+    }
+}
---
+++ Models/reserva.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P01_2022_SS_650_2021_OF_601.Models
+{
+    public class reserva
+    {
+        [Key]
+        public int id_reserva { get; set; }
+        public DateTime fecha { get; set; }
+        public DateTime hora_reserva { get; set; }
+        public int horas { get; set; }
+        public bool estado { get; set; }
+        public int id_usuario { get; set; }
+    }
+}
