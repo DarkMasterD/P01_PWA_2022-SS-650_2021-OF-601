dff64c8c2c9ec27f470f6f8ea5921bb448aa8147
Rafael Sandoval <rafael.sandoval@catolica.edu.sv>
2025-02-24 20:59:35 -0600

    creacion de los controladores de reserva y usuario y sus metodos

---
+++ Controllers/reservaController.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using P01_2022_SS_650_2021_OF_601.Models;
+
+namespace P01_2022_SS_650_2021_OF_601.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class reservaController : ControllerBase
+    {
+        private readonly parqueoContext _parqueoContext;
+
+        public reservaController(parqueoContext parqueoContext)
+        {
+            _parqueoContext = parqueoContext;
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult GuardarReserva([FromBody] reserva reserva)
+        {
+            try
+            {
+                _parqueoContext.reserva.Add(reserva);
+                _parqueoContext.SaveChanges();
+                return Ok(reserva);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("GetById/{id}")]
+        public IActionResult Get(int id)
+        {
+            var reservas = (from r in _parqueoContext.reserva
+                            join u in _parqueoContext.usuario on r.id_usuario equals u.id_usuario
+                            where r.id_usuario == id
+                            select new
+                            {
+                                u.nombre,
+                                r.fecha,
+                                r.hora_reserva,
+                                r.horas
+                            }).ToList();
+
+            if (!reservas.Any())
+            {
+                return NotFound("No se encontraron reservas para este usuario.");
+            }
+            return Ok(reservas);
+        }
+
+
+        [HttpDelete]
+        [Route("CancelarReserva/{id}")]
+        public IActionResult CancelarReserva(int id)
+        {
+            var reserva = _parqueoContext.reserva.FirstOrDefault(r => r.id_reserva == id);
+
+            if (reserva == null)
+            {
+                return NotFound("Reserva no encontrada.");
+            }
+
+            var ahora = DateOnly.FromDateTime(DateTime.Now);
+            var horaActual = TimeOnly.FromDateTime(DateTime.Now);
+
+            if (reserva.fecha < ahora || (reserva.fecha == ahora && reserva.hora_reserva <= horaActual))
+            {
+                return BadRequest("No se puede cancelar una reserva que ya ha iniciado.");
+            }
+
+            try
+            {
+                reserva.estado = 'C';
+                _parqueoContext.SaveChanges();
+                return Ok("Reserva cancelada exitosamente.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpGet]
+        [Route("EspaciosReservadosPorDia")]
+        public IActionResult EspaciosReservadosPorDia([FromQuery] DateTime fecha)
+        {
+            try
+            {
+                var fechaSinHora = DateOnly.FromDateTime(fecha);
+
+                var reservas = _parqueoContext.reserva
+                    .Where(r => r.fecha == fechaSinHora) 
+                    .ToList();
+
+                return Ok(reservas);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpGet]
+        [Route("EspaciosReservadosEntreFechas")]
+        public IActionResult ObtenerEspaciosReservadosEntreFechas([FromQuery] DateTime fechaInicio, [FromQuery] DateTime fechaFin)
+        {
+            try
+            {
+                var fechaInicioOnly = DateOnly.FromDateTime(fechaInicio); 
+                var fechaFinOnly = DateOnly.FromDateTime(fechaFin);      
+
+                var reservas = _parqueoContext.reserva
+                    .Where(r => r.fecha >= fechaInicioOnly && r.fecha <= fechaFinOnly) 
+                    .ToList();
+
+                return Ok(reservas);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+    }
+}
---
+++ Controllers/usuarioController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P01_2022_SS_650_2021_OF_601.Models;
+
+namespace P01_2022_SS_650_2021_OF_601.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class usuarioController : ControllerBase
+    {
+        private readonly parqueoContext _parqueoContext;
+
+        public usuarioController(parqueoContext parqueoContext)
+        {
+            _parqueoContext = parqueoContext;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            List<usuario> listadoUsuarios = (from u in _parqueoContext.usuario
+                                              select u).ToList();
+
+            if (listadoUsuarios.Count() == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(listadoUsuarios);
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult GuardarUsuario([FromBody] usuario usuario)
+        {
+            try
+            {
+                _parqueoContext.usuario.Add(usuario);
+                _parqueoContext.SaveChanges();
+                return Ok(usuario);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("actualizar/{id}")]
+        public IActionResult ActualizarUsuario(int id, [FromBody] usuario usuarioModificar)
+        {
+            var usuarioActual = (from u in _parqueoContext.usuario
+                                 where u.id_usuario == id
+                                 select u).FirstOrDefault();
+
+            if (usuarioActual == null)
+            {
+                return NotFound();
+            }
+
+            usuarioActual.nombre = usuarioModificar.nombre;
+            usuarioActual.telefono = usuarioModificar.telefono;
+            usuarioActual.contrasenia = usuarioModificar.contrasenia;
+
+            _parqueoContext.Entry(usuarioActual).State = EntityState.Modified;
+            _parqueoContext.SaveChanges();
+
+            return Ok(usuarioModificar);
+        }
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+        public IActionResult EliminarUsuario(int id)
+        {
+            var usuario = (from u in _parqueoContext.usuario
+                           where u.id_usuario == id
+                           select u).FirstOrDefault();
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            _parqueoContext.usuario.Attach(usuario);
+            _parqueoContext.usuario.Remove(usuario);
+            _parqueoContext.SaveChanges();
+
+            return Ok(usuario);
+        }
+
+        [HttpGet]
+        [Route("GetCredenciales")]
+        public IActionResult GetCredenciales(string nombre = null, string contrasenia = null)
+        {
+            var usuario = from u in _parqueoContext.usuario
+                          select u;
+
+            if (!string.IsNullOrEmpty(nombre))
+            {
+                usuario = usuario.Where(u => u.nombre.Contains(nombre));
+            }
+            if (!string.IsNullOrEmpty(contrasenia))
+            {
+                usuario = usuario.Where(u => u.contrasenia.Contains(contrasenia));
+            }
+
+            var usuariofiltrado = usuario.ToList();
+
+            if (usuariofiltrado.Count == 0)
+            {
+                return NotFound("Credenciales incorrectas.");
+            }
+
+            return Ok("Credenciales correctas");
+        }
+    }
+}
